cabfd42d21980f5c689198cc41b8a4df44071af0
awanti_19 <awanti.deotale@vphrase.com>
2021-02-11 21:27:04 +0530

    R7. Extracted method for `total frequent renter points` and `total rental charges`.

--- Refactoring/src/customer.py
+++ Refactoring/src/customer.py
@@ -9,21 +9,34 @@ class Customer:
     def add_rental(self, rental):
         self.__rentals.append(rental)
 
+    def get_total_frequent_renter_points(self):
+        total_renter_points = 0
+        for rental in self.__rentals:
+            if rental.get_days_rented() > 0:
+                total_renter_points += rental.get_frequent_renter_points()
+
+        return total_renter_points
+
+    def get_total_amount(self):
+        total_amount = 0
+        for rental in self.__rentals:
+            if rental.get_days_rented() > 0:
+                total_amount += rental.get_charge()
+
+        return total_amount
+
     def get_statement(self):
-        totalAmount = 0
-        frequentRenterPoints = 0
+
         result = f"Rental Record for {self.__name}\n"
         for rental in self.__rentals:
 
-            # add frequent renter points
-            frequentRenterPoints += rental.get_frequent_renter_points()
+            if rental.get_days_rented() > 0:
 
-            # show figures for this rental
-            result += f" {rental.get_movie().get_title()}: {str(rental.get_charge())}\n"
-            totalAmount += rental.get_charge()
+                # show figures for this rental
+                result += f" {rental.get_movie().get_title()}: {str(rental.get_charge())}\n"
 
         # add footer lines
-        result += f" Amount owed is {str(totalAmount)} \n"
-        result += f" You earned {str(frequentRenterPoints)} frequent renter points \n"
+        result += f" Amount owed is {str(self.get_total_amount())} \n"
+        result += f" You earned {str(self.get_total_frequent_renter_points())} frequent renter points \n"
 
-        return result, totalAmount, frequentRenterPoints
+        return result, self.get_total_amount(), self.get_total_frequent_renter_points()
